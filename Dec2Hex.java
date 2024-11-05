542e45c972d0c66a99a268f1e1c436de9367e0be
ChristieSeery <cseery300@caledonian.ac.uk>
2024-11-05 14:13:41 +0000

    Fixed Java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -8,7 +8,7 @@ class Dec2Hex
         try{
             Arg1 = Integer.parseInt(args[0]);
         } catch (NumberFormatException e) {
-            System.out.println("Please enter an Integer!");
+            System.out.println("Please enter an Integer");
         }
 
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
