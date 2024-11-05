1527d167fa8fab52d83b8a71cf273eaad8c732c0
ChristieSeery <cseery300@caledonian.ac.uk>
2024-11-05 13:28:24 +0000

    Fixed Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -3,9 +3,14 @@ import java.util.Scanner;
 
 class Dec2Hex
 {
-public static int Arg1;
-    public static void main(String args[])    {
-        Arg1 = Integer.parseInt(args[0]);
+    public static int Arg1;
+    public static void main(String args[]){
+        try{
+            Arg1 = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter an Integer");
+        }
+
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
@@ -23,4 +28,3 @@ public static int Arg1;
 
     }
 }
-
